b3fd27b3d8ee1be4d0ad87cce9972f441f8fbea5
Marcelee Laylay <youmustbeajoke@gmail.com>
2023-04-24 16:17:18 +0800

    First commit and Added the Ship Factory function with test

---
+++ src/Ship.js
@@ -0,0 +1,51 @@
+export default function Ship(shipArrayCoordinate = [], hits = 0) {
+  let shipCoordinate = shipArrayCoordinate;
+  let sunk = false;
+  const length = shipCoordinate.length;
+  let shipHits = hits;
+
+  // From what I know also about this ship is
+  // This factory function should have parameter of the hits and the length of the ship
+
+  function hit(attackCoordinate) {
+    // On what I know
+    // When a computer hits A3 and then in the shipCoordinate array contains that coordinateAttack
+    // Our 'hits' will increment
+    // If not return a miss
+
+    // And in the gameboard UI, I think... That I don't have to worry about the duplicate coordinate attack?
+    // If the coordinate has already been hit I wouldn't let the player hit it again? Right?
+
+    if (shipCoordinate.includes(attackCoordinate)) {
+      shipHits += 1;
+
+      console.log(
+        `You hit ${attackCoordinate}!, He now only have ${length - shipHits}`
+      );
+      return length - shipHits;
+    }
+
+    return "You miss!";
+  }
+
+  function isSunk() {
+    if (shipHits === length) {
+      sunk = true;
+      return "The ship has sunk!!";
+    }
+
+    return `You still have ${length - shipHits} ships coordinates left.`;
+  }
+
+  function getShipHits() {
+    return shipHits;
+  }
+
+  return {
+    shipCoordinate,
+    length,
+    getShipHits,
+    hit,
+    isSunk,
+  };
+}
---
+++ test/Ship.test.js
@@ -0,0 +1,24 @@
+import Ship from "../src/Ship";
+
+test("A Ship Object", () => {
+  const shipCoordinate = ["A2", "A3", "A4"];
+  const firstAttack = "A3";
+  const secondAttack = "A2";
+  const thirdAttack = "A4";
+
+  const missedAttackCoordinate = "B3";
+
+  const cruiserShip = Ship(shipCoordinate);
+
+  expect(cruiserShip.length).toBe(3);
+  expect(cruiserShip.hit(firstAttack)).toBe(2);
+  expect(cruiserShip.isSunk()).toBe(
+    `You still have ${2} ships coordinates left.`
+  );
+  expect(cruiserShip.getShipHits()).toBe(1);
+  expect(cruiserShip.hit(missedAttackCoordinate)).toBe("You miss!");
+  expect(cruiserShip.hit(secondAttack)).toBe(1);
+  expect(cruiserShip.getShipHits()).toBe(2);
+  expect(cruiserShip.hit(thirdAttack)).toBe(0);
+  expect(cruiserShip.isSunk()).toBe("The ship has sunk!!");
+});
